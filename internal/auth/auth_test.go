a9ef42d1255c22fa5797b3c415cdf1c697dd6419
arturogood17 <arturo_good17@hotmail.com>
2025-07-12 13:53:08 +0200

    Unit tests for GetApiKey

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,29 +1,51 @@
 package auth
 
-import("testing"
-"net/http")
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
 
-
-func TestAuth(t *testing.T){
-	tests := []string{
-		input http.Header
-		want string
-		err error
+func TestAuth(t *testing.T) {
+	tests := []struct {
+		key   string
+		value string
+		want  string
+		err   string
 	}{
 		{
-			input: http.Header{"Authorization": "ApiKey 12345"},
-			want: "",
-			err: nil,
+			key:   "Authorization",
+			value: "ApiKey 12345",
+			want:  "12345",
+			err:   "",
 		},
 		{
-			input: http.Header{"A": "ApiKey 12345"},
-			want: "12345",
-			err: errors.New("no authorization header included"),
+			key:   "A",
+			value: "ApiKey 12345",
+			want:  "",
+			err:   "no authorization header included",
 		},
 		{
-			input: http.Header{"Authorization": "12345"},
-			want: "12345",
-			err: errors.New("malformed authorization header"),
+			key:   "Authorization",
+			value: "12345",
+			want:  "",
+			err:   "malformed authorization header",
 		},
+	}
+
+	for i, test := range tests {
+		h := http.Header{}
+		t.Run(fmt.Sprintf("Test %v", i), func(t *testing.T) {
+			h[test.key] = []string{test.value}
+			got, err := GetAPIKey(h)
+			if err != nil {
+				if err.Error() != test.err {
+					t.Errorf("unexpected error")
+				}
+			}
+			if got != test.want {
+				t.Fatalf("got: %v, want: %v", got, test.want)
+			}
+		})
 	}
 }
